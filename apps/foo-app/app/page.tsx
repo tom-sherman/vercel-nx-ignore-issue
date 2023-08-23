f51cf5d0e9d0d679233dac2311a76201bfe8aeae
Tom Sherman <the.tomsherman@gmail.com>
2023-08-23 11:41:49 +0100

    Change foo app

--- apps/foo-app/app/page.tsx
+++ apps/foo-app/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Index() {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there, </span>
+              <span> Hello there! </span>
               Welcome foo-app 👋
             </h1>
           </div>
